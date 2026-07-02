353ada46ec8799012e1aabd5799dab7b25871232
Gv1don <crgvidonlol@gmail.com>
2026-07-02 15:22:52 +0300

    Fixed basic URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,25 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+            URL::forceRootUrl($scheme . '://' . $_SERVER['HTTP_HOST']);
+
+            if ($scheme === 'https') {
+                URL::forceScheme('https');
+            }
+        }
     }
 }
